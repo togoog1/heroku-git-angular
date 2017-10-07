69da7f71716d3705970c1dc3597cbf8b7bd7662c
togoog1 <nathanallen888@gmail.com>
2017-10-07 03:07:15 -0500

    1st commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public dt:Date = new Date();
+  private minDate:Date = null;
+  private events:Array<any>;
+  private tomorrow:Date;
+  private afterTomorrow:Date;
+  private formats:Array<string> = ['DD-MM-YYYY', 'YYYY/MM/DD', 'DD.MM.YYYY', 'shortDate'];
+  private format = this.formats[0];
+  private dateOptions:any = {
+    formatYear: 'YY',
+    startingDay: 1
+  };
+  private opened:boolean = false;
+
+  constructor() {}
+
+  public getDate():number {
+    return this.dt && this.dt.getTime() || new Date().getTime();
+  }
+
+}
